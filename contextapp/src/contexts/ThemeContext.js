11828882a5467580a9f6fdd3d35131703788e92e
TarekLH <lahouaouitarek99@gmail.com>
2023-06-23 01:46:53 +0100

    correction for fuctional composent version of ThemeContext

--- contextapp/src/contexts/ThemeContext.js
+++ contextapp/src/contexts/ThemeContext.js
@@ -1,46 +1,20 @@
-import React, { Component, createContext, useState } from 'react';
+import React, { createContext, useState } from 'react';
 
 export const ThemeContext = createContext();
 
-class ThemeContextProvider extends Component {
-  state = {
+export default function ThemeContextProvider({children}) {
+  const [themeState, setThemeState] = useState({
     isLightTheme: true,
     light: { syntax: '#555', ui: '#ddd', bg: '#eee'},
     dark: { syntax: '#ddd', ui: '#333', bg: '#555'}
-  };
-  toggleTheme = () => {
-    this.setState({ isLightTheme: !this.state.isLightTheme});
+  });
+  const toggleTheme = () => {
+    setThemeState({...themeState, isLightTheme: !themeState.isLightTheme});
   };
 
-  render() {
-    return (
-      <ThemeContext.Provider value={{...this.state, toggleTheme: this.toggleTheme}}>
-        {this.props.children}
-      </ThemeContext.Provider>
-    );
-  }
+  return (
+    <ThemeContext.Provider value={{...themeState, toggleTheme}}>
+      {children}
+    </ThemeContext.Provider>
+  )
 }
-
-export default ThemeContextProvider;
-
-//! Functional component version not working
-// export const ThemeContext = createContext();
-
-// export default function ThemeContextProvider({children}) {
-//   const [themeState, setThemeState] = useState({
-//     isLightTheme: true,
-//     light: { syntax: '#555', ui: '#ddd', bg: '#eee'},
-//     dark: { syntax: '#ddd', ui: '#333', bg: '#555'}
-//   });
-//   const toggleTheme = () => {
-//     setThemeState({isLightTheme: !themeState.isLightTheme});
-//   };
-//   console.log(themeState)
-//   console.log({...themeState})
-
-//   return (
-//     <ThemeContext.Provider value={{...themeState, toggleTheme}}>
-//       {children}
-//     </ThemeContext.Provider>
-//   )
-// }
